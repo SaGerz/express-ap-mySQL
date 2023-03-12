e9741efb9b305bbda95b0676143522998f575686
SaGerz <75112010+SaGerz@users.noreply.github.com>
2023-03-12 20:16:51 +0700

    Update Connection

--- src/Connection/Connection.js
+++ src/Connection/Connection.js
@@ -1,10 +1,6 @@
 const mysql = require('mysql')
 
 const db = mysql.createConnection({
-    // host: "localhost",
-    // user: "root",
-    // password: "",
-    // database: "siakad"
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
